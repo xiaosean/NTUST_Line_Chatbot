0b711297c49957874b35c289bca9341a560e4859
xiaosean <xiaosean5408@gmail.com>
2018-04-18 02:33:03 +0800

    my token

---
+++ my_token.py
@@ -0,0 +1,7 @@
+class MyToken(object):
+	"""for store key"""
+	Channel_ID = "1573578034"
+	Channel_SECRET = "5c06d4019b4a9c969fa478fe9fcbb196"
+	def __init__(self, arg):
+		super(MyToken, self).__init__()
+	
