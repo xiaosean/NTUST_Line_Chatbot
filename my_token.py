9150c3bc30943ca46db455bc63ef15f08ccdc73e
xiaosean <xiaosean5408@gmail.com>
2018-04-18 02:56:45 +0800

    my token new secret

--- my_token.py
+++ my_token.py
@@ -2,7 +2,7 @@ class MyToken(object):
 	"""for store key"""
 	CHANNEL_ACCESS_TOKEN = "DXIiKVkPVYbb2wA15fDuOAtM3ryk5Efpa3YimnGBUHppFeQSMxQZvo7ApW4ObttE9CRNDeXrHIIYPf1jcu2JzhyhOwQ0Tpc4KhVyOnqC7XOTr5be+1TYNyTtebi4TndKiQHB96fsaxzW0oVs2Zd5RgdB04t89/1O/w1cDnyilFU="
 	Channel_ID = "1573578034"
-	Channel_SECRET = "5c06d4019b4a9c969fa478fe9fcbb196"
+	Channel_SECRET = "61609081efb5e2c9bae521c2d8e42013"
 	def __init__(self, arg):
 		super(MyToken, self).__init__()
 	
