3fbd0afc1f7a6e1233bbf50a922898db27712937
xiaosean <xiaosean5408@gmail.com>
2018-04-24 22:45:50 +0800

    token

--- my_token.py
+++ my_token.py
@@ -1,8 +1,8 @@
 class MyToken(object):
-	"""for store key"""
-	CHANNEL_ACCESS_TOKEN = "DXIiKVkPVYbb2wA15fDuOAtM3ryk5Efpa3YimnGBUHppFeQSMxQZvo7ApW4ObttE9CRNDeXrHIIYPf1jcu2JzhyhOwQ0Tpc4KhVyOnqC7XOTr5be+1TYNyTtebi4TndKiQHB96fsaxzW0oVs2Zd5RgdB04t89/1O/w1cDnyilFU="
-	Channel_ID = "1573578034"
-	Channel_SECRET = "61609081efb5e2c9bae521c2d8e42013"
-	def __init__(self, arg):
-		super(MyToken, self).__init__()
-	
+    """for store key"""
+    CHANNEL_ACCESS_TOKEN = "DXIiKVkPVYbb2wA15fDuOAtM3ryk5Efpa3YimnGBUHppFeQSMxQZvo7ApW4ObttE9CRNDeXrHIIYPf1jcu2JzhyhOwQ0Tpc4KhVyOnqC7XOTr5be+1TYNyTtebi4TndKiQHB96fsaxzW0oVs2Zd5RgdB04t89/1O/w1cDnyilFU="
+    CHANNEL_ID = "1573578034"
+    CHANNEL_SECRET = "61609081efb5e2c9bae521c2d8e42013"
+    def __init__(self, arg):
+        super(MyToken, self).__init__()
+    
